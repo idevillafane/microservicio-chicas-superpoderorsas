2ef56d3260fac63b89774e5f6df04c74f4eecdb4
Ignacio de Villafañe <idevillafane@gmail.com>
2022-12-09 13:44:32 -0300

    Primeros cambios para aplicar Eureka

--- bombon-service/src/main/java/com/nacho/bombonservice/BombonServiceApplication.java
+++ bombon-service/src/main/java/com/nacho/bombonservice/BombonServiceApplication.java
@@ -2,6 +2,7 @@ package com.nacho.bombonservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class BombonServiceApplication {
